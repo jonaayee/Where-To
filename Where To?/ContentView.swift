7f4a696de0a43c380d3aff75d9d15b10a20c439d
jonaayee <108772106+jonaayee@users.noreply.github.com>
2025-02-17 21:57:52 -0500

    Initial Commit

---
+++ Where To?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Where To?
+//
+//  Created by Jonathan Amburgy on 2/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Where To?/Where_To_App.swift
@@ -0,0 +1,17 @@
+//
+//  Where_To_App.swift
+//  Where To?
+//
+//  Created by Jonathan Amburgy on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Where_To_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
